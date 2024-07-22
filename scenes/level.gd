6154ec9e502290f02a55b3e18bf2a98afd685518
Darko Cirkovic <humanbones@live.com>
2024-07-22 22:00:47 +0200

    finishing the project

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const MAIN_MENU = preload("res://scenes/main_menu/main_menu.tscn")
 const ANIMAL = preload("res://scenes/Animal/animal.tscn")
 @onready var animal_start = $AnimalStart
 
@@ -7,6 +8,10 @@ func _ready():
 	spawn_animal()
 	SignalManager.on_animal_died.connect(spawn_animal)
 
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_packed(MAIN_MENU)
 
 func spawn_animal() ->void:
 	var animal_instance = ANIMAL.instantiate() as Node2D
